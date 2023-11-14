5d5523180dfe7fb089e091f2032b093d95a8d390
nicolasgodineau <nicolasgodineau@live.fr>
2023-11-14 11:10:47 -0500

    Modification du titre de la page (SEO)

--- src/pages/index.js
+++ src/pages/index.js
@@ -103,11 +103,6 @@ const IndexPage = () => {
   )
 }
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
-export const Head = () => <Seo title="Home" />
+export const Head = () => <Seo title="Noëmie Lemoine - Make-up artist" />
 
 export default IndexPage
