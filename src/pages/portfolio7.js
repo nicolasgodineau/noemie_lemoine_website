b3d2035590a23acf7b2a4fb61ab9bd2bd0eb106d
nicolasgodineau <nicolasgodineau@live.fr>
2023-11-15 10:52:13 -0500

    Création de la page 7 du portfolio

---
+++ src/pages/portfolio7.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+import { useTheme } from "@mui/material/styles"
+import { StaticImage } from "gatsby-plugin-image"
+import { Container } from "@mui/material"
+
+function Portfolio7Page() {
+    const theme = useTheme()
+    return (
+        <Container
+            maxWidth={false}
+            disableGutters
+            sx={{
+                height: "100svh",
+                display: "flex",
+                gap: "5rem",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "black",
+            }}>
+            <StaticImage
+                src="../images/portfolio/img_portfolio_13.webp"
+                alt="image d'illustration"
+                width={500}
+                height={750}
+                objectFit="contain"
+            />
+        </Container>
+    )
+}
+
+export default Portfolio7Page
