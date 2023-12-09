79d57c9df9ccca33e28cfd94cf0b34e5e69557da
nicolasgodineau <nicolasgodineau@live.fr>
2023-12-09 18:15:52 -0500

    Début de la création du formulaire de contact

--- src/components/header.js
+++ src/components/header.js
@@ -11,44 +11,48 @@ const Header = () => {
   const links = [
     { href: "/#portfolio", name: "Portfolio" },
     { href: "/page2", name: "À propos" },
-    { href: "/page3", name: "Contact" },
+    { href: "/contact", name: "Contact" },
     { href: "/portfolioTest", name: "Mariage" },
   ]
 
   return (
     <Container
-      maxWidth="xl"
+      maxWidth="false"
       sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        paddingTop: "1rem",
         backgroundColor: theme.palette.dark,
         color: theme.palette.light,
       }}
     >
-      {links.map((link, index) => (
-        <Link
-          key={index}
-          to={link.href}
-          style={
-            /* Style pour le dernier élément, qui est le portfolio */
-            index === links.length - 1 ? { position: "absolute", top: 70 } : {}
-          }
-        >
-          <Button
-            color="secondary"
-            sx={{
-              fontFamily: "Gowun Batang",
-              textTransform: "none",
-              letterSpacing: "1.6px",
-              fontSize: "1rem",
-              color: theme.palette.light,
-            }}
+      <Container maxWidth="lg"
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          paddingTop: "1rem",
+        }}>
+        {links.map((link, index) => (
+          <Link
+            key={index}
+            to={link.href}
+            style={
+              /* Style pour le dernier élément, qui est le portfolio */
+              index === links.length - 1 ? { position: "absolute", top: 70 } : {}
+            }
           >
-            {link.name}
-          </Button>
-        </Link>
-      ))}
+            <Button
+              color="secondary"
+              sx={{
+                fontFamily: "Gowun Batang",
+                textTransform: "none",
+                letterSpacing: "1.6px",
+                fontSize: "1rem",
+                color: theme.palette.light,
+              }}
+            >
+              {link.name}
+            </Button>
+          </Link>
+        ))}
+      </Container>
     </Container>
   )
 }
