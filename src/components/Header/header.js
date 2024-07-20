65f7c86fe2cc4d4c75300d5621de326c7f1e5879
nicolasgodineau <nicolasgodineau@live.fr>
2024-07-20 17:31:29 -0400

    clean code

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,14 +3,8 @@ import React, { useState } from "react";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
 import { Container } from "@mui/material";
 
-import MenuIcon from '@mui/icons-material/Menu';
-
-import MenuMobile from "@components/menu/menuMobile.js";
-import MenuDesktop from "@components/menu/menuDesktop.js";
 import Menu from "@components/menu/menu.js";
 
 const Header = ({ color, colorText }) => {
